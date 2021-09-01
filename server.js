21a7d4567343bb2235065c3f6db16463fd41e7a6
Zachary Dowd <zdowd2796@gmail.com>
2021-09-01 14:53:01 -0600

    added 404 and wildcard

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/404.html'))
+});
+
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
